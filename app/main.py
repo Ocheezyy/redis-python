74e4dcbacd3aa8c19c9355f38d8fb09a78822dcd
ocheezyy <miniod2018@gmail.com>
2024-06-05 14:01:21 -0400

    Added request body checking to handle different request types

--- app/main.py
+++ app/main.py
@@ -5,19 +5,34 @@ PORT = 6379
 CRLF = "\r\n"
 PONG = f"+PONG{CRLF}"
 
-REDIS_SIG = f"*1{CRLF}$4{CRLF}"
-PING = f"{REDIS_SIG}PING{CRLF}"
+RESP_BULK_SIG = f"$3{CRLF}"
 
+REDIS_PING_SIG = f"*1{CRLF}$4{CRLF}"
+PING = f"{REDIS_PING_SIG}PING{CRLF}"
+REDIS_ECHO_SIG = f"*2{CRLF}$4{CRLF}ECHO{CRLF}"
+
+
+def ping() -> bytes:
+    return PONG.encode("utf8")
+
+def echo(request_str: str) -> bytes:
+    return request_str.replace(REDIS_ECHO_SIG, "").encode("utf8")
 
 async def handle_client(reader, writer):
-    request = None
+    request = ""
     while request != 'quit':
-        request = (await reader.read(255)).decode('utf8')
+        request: str = (await reader.read(255)).decode('utf8')
         print(f"{request=}")
         if not request:
             print("No data received")
             return
-        writer.write(PONG.encode('utf8'))
+
+        if request.startswith(REDIS_PING_SIG):
+            writer.write(ping())
+        elif request.startswith(REDIS_ECHO_SIG):
+            writer.write(echo(request))
+        else:
+            return
         await writer.drain()
     writer.close()
 
