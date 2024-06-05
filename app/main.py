42e65243721017be0b1d8daf31f5305bd5af428d
ocheezyy <miniod2018@gmail.com>
2024-06-05 15:05:31 -0400

    Removed unused constants. Renamed methods to be specific to redis. Added encoder for response encoding. Added split for ease of request reading

--- app/main.py
+++ app/main.py
@@ -1,45 +1,66 @@
 import asyncio
+from asyncio.streams import StreamWriter
 
 HOST = "localhost"
 PORT = 6379
 CRLF = "\r\n"
 PONG = f"+PONG{CRLF}"
 
-RESP_BULK_SIG = f"$3{CRLF}"
 
-REDIS_PING_SIG = f"*1{CRLF}$4{CRLF}"
-PING = f"{REDIS_PING_SIG}PING{CRLF}"
-REDIS_ECHO_SIG = f"*2{CRLF}$4{CRLF}ECHO{CRLF}"
+def redis_ping() -> bytes:
+    return redis_encode("PONG")
 
 
-def ping() -> bytes:
-    return PONG.encode("utf8")
+def redis_echo(req_arr: list[str]) -> bytes:
+    return redis_encode([el for el in req_arr[4:5]])
 
-def echo(request_str: str) -> bytes:
-    return request_str.replace(REDIS_ECHO_SIG, "").encode("utf8")
 
-async def handle_client(reader, writer):
+def redis_encode(data, encoding="utf-8"):
+    if not isinstance(data, list):
+        data = [data]
+    size = len(data)
+    encoded = []
+    for datum in data:
+        encoded.append(f"${len(datum)}")
+        encoded.append(datum)
+    if size > 1:
+        encoded.insert(0, f"*{size}")
+    print(f"encoded: {encoded}")
+    return (CRLF.join(encoded) + CRLF).encode(encoding=encoding)
+
+
+async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     request = ""
     while request != 'quit':
-        request: str = (await reader.read(255)).decode('utf8')
+        request: str = (await reader.read(255)).decode()
         print(f"{request=}")
         if not request:
-            print("No data received")
+            # print("No data received")
             return
 
-        if request.startswith(REDIS_PING_SIG):
-            writer.write(ping())
-        elif request.startswith(REDIS_ECHO_SIG):
-            writer.write(echo(request))
+        req_arr: list[str] = request.split(CRLF)
+        req_arr_size = len(req_arr)
+        print(f"req_arr: ${req_arr}")
+
+        response: bytes
+        if req_arr[2].lower() == "ping":
+            response = redis_ping()
+        elif req_arr[2].lower() == "echo":
+            response = redis_echo(req_arr)
         else:
             return
+
+        print(f"response: {response}")
+        writer.write(response)
         await writer.drain()
     writer.close()
+
 
 async def run_server():
     server = await asyncio.start_server(handle_client, HOST, PORT)
     async with server:
         await server.serve_forever()
+
 
 if __name__ == "__main__":
     asyncio.run(run_server())
