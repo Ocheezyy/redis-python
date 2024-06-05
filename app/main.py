3ab5f6a5786090439d3a2e65b8459e471991559a
ocheezyy <miniod2018@gmail.com>
2024-06-05 01:21:19 -0400

    Added threading to main event loop

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 HOST = "localhost"
 PORT = 6379
 CRLF = "\r\n"
@@ -8,17 +10,19 @@ REDIS_SIG = f"*1{CRLF}$4{CRLF}"
 PING = f"{REDIS_SIG}PING{CRLF}"
 
 
+def req_handler(conn: socket.socket, addr):
+    data = conn.recv(1024)
+    if not data:
+        return
+    print(f"{data=}")
+    conn.sendall(b"+PONG\r\n")
+
+
 def main():
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    conn, addr = server_socket.accept()  # wait for client
-    with conn:
-        # loop
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f"{data=}")
-            conn.sendall(b"+PONG\r\n")
+    while True:
+        conn, addr = server_socket.accept()  # wait for client
+        threading.Thread(target=req_handler, args=(conn, addr)).start()
 
 
 if __name__ == "__main__":
